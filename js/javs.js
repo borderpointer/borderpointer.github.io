c9d021e1ed3e39fcaa80e66591758b2b84e81cd1
Elli Park <ellijpark@gmail.com>
2016-09-20 13:21:36 -0400

    :scissors: whitespace

--- js/javs.js
+++ js/javs.js
@@ -1,15 +1,7 @@
-console.log("js linked");
-
 $(function() {
-
     $('.video').mouseenter(function() {
-
         if (this.paused) this.play();
-
     }).mouseleave(function() {
-
         if (!this.paused) this.pause();
-
     });
-
 });
